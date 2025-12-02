de8f6e0bd78f8d7bf0e35e30a67a49b5e7ecb2f9
Max Philips <mphilips@copper.com>
2025-12-01 19:05:03 -0600

    01 done working for the day

--- 01.go
+++ 01.go
@@ -2,28 +2,15 @@ package main
 
 import (
 	"bufio"
-	"container/list"
 	"fmt"
 	"log"
 	"os"
+	"strings"
+	"strconv"
+	"unicode/utf8"
 )
 
 func main() {
-	// https://pkg.go.dev/container/list
-	l := list.New()
-	for i := range 101 {
-		l.PushBack(i)
-	}
-
-	// To achieve circular traversal, you need to handle the end conditions
-	// explicitly. When traversing forward, if e.Next() becomes nil, you can reset
-	// e to l.Front(). Similarly, for backward traversal, if e.Prev() becomes nil, you can reset e to l.Back().
-
-	arrow := l.Front()
-	fmt.Println(arrow.Value)
-	arrow.turnRight(50)
-	fmt.Println(arrow.Value)
-
 	// https://stackoverflow.com/questions/8757389/reading-a-file-line-by-line-in-go
 	file, err := os.Open("01.txt")
 	if err != nil {
@@ -31,27 +18,55 @@ func main() {
 	}
 	defer file.Close()
 
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		fmt.Println(scanner.Text())
-	}
-
 	numZeroes := 0
 	fmt.Println(numZeroes)
+
+	dial := 50
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		// assume perfect input - "R" prefix means travel positively...
+		direction := 1
+
+		// ...and "L" prefix means travel negatively
+		if strings.HasPrefix(line, "L") {
+			direction = -1
+		}
+
+		line = trimFirstRune(line)
+
+		i, err := strconv.Atoi(line)
+		if err != nil {
+			panic(err)
+		}
+
+		clicks := i * direction
+
+		dial = turn(dial, clicks)
+		fmt.Println(dial)
+	}
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
 }
 
-func (e *Element) turnRight(clicks int) Element {
-	for i := range clicks {
-		e = e.Next()
-	}
+func turn(val int, clicks int) int {
+	// don't care about the first
+	// lmao actually we do
+	clicks = clicks % 100
+	val = val + clicks
 
-	return e
+	return val
 }
 
-// func (e *Element) turnLeft(clicks int) Element {
+// Source - https://stackoverflow.com/a/48801414
+// Posted by user5728991, modified by community. See post 'Timeline' for change history
+// Retrieved 2025-12-01, License - CC BY-SA 4.0
 
-// }
+func trimFirstRune(s string) string {
+	_, i := utf8.DecodeRuneInString(s)
+	return s[i:]
+}
