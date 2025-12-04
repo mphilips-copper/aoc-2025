4e5c2cea114dcdb6df4d30b508ff223f27619b50
Max Philips <mphilips@copper.com>
2025-12-04 15:18:22 -0600

    03 part two

--- 03.go
+++ 03.go
@@ -5,7 +5,6 @@ import (
 	"log"
 	"os"
 	"strings"
-	"unicode/utf8"
 )
 
 func main() {
@@ -18,24 +17,40 @@ func main() {
 	joltage := 0
 
 	for _, batteryBank := range batteryBanks {
-		// can never use the last rune as the first digit
-		trimmedBatteryBank := trimLastRune(batteryBank)
+		lD12, index := findLargestDigitAndIndex(0, trimLastNChars(batteryBank, 11))
+		lD11, index := findLargestDigitAndIndex(index+1, trimLastNChars(batteryBank, 10))
+		lD10, index := findLargestDigitAndIndex(index+1, trimLastNChars(batteryBank, 9))
+		lD9, index := findLargestDigitAndIndex(index+1, trimLastNChars(batteryBank, 8))
+		lD8, index := findLargestDigitAndIndex(index+1, trimLastNChars(batteryBank, 7))
+		lD7, index := findLargestDigitAndIndex(index+1, trimLastNChars(batteryBank, 6))
+		lD6, index := findLargestDigitAndIndex(index+1, trimLastNChars(batteryBank, 5))
+		lD5, index := findLargestDigitAndIndex(index+1, trimLastNChars(batteryBank, 4))
+		lD4, index := findLargestDigitAndIndex(index+1, trimLastNChars(batteryBank, 3))
+		lD3, index := findLargestDigitAndIndex(index+1, trimLastNChars(batteryBank, 2))
+		lD2, index := findLargestDigitAndIndex(index+1, trimLastNChars(batteryBank, 1))
+		lD1, _ := findLargestDigitAndIndex(index+1, batteryBank)
 
-		// find the largest digit from trimmed battery bank
-		largestDigitTens, index := findLargestDigitAndIndex(0, trimmedBatteryBank)
-
-		// find the largest digit to its right in battery bank
-		largestDigitOnes, _ := findLargestDigitAndIndex(index+1, batteryBank)
-
-		joltage = joltage + largestDigitTens*10 + largestDigitOnes
+		// lol, lmao even
+		joltage = joltage +
+			lD12*100000000000 +
+			lD11*10000000000 +
+			lD10*1000000000 +
+			lD9*100000000 +
+			lD8*10000000 +
+			lD7*1000000 +
+			lD6*100000 +
+			lD5*10000 +
+			lD4*1000 +
+			lD3*100 +
+			lD2*10 +
+			lD1
 	}
 
 	fmt.Println(joltage)
 }
 
-func trimLastRune(s string) string {
-	_, i := utf8.DecodeLastRuneInString(s)
-	return s[:len(s)-i]
+func trimLastNChars(s string, n int) string {
+	return s[:len(s)-n]
 }
 
 func findLargestDigitAndIndex(startAt int, batteryBank string) (int, int) {
@@ -51,5 +66,5 @@ func findLargestDigitAndIndex(startAt int, batteryBank string) (int, int) {
 		}
 	}
 
-	return largestDigit, index
+	return largestDigit, startAt + index
 }
